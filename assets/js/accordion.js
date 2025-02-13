dce17dda9d93ce7001d9ea2d45f72238bfabbd64
Rashids00 <rashids24680@gmail.com>
2025-02-13 19:12:55 +0530

    fix spacing

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -7,24 +7,12 @@
             var content = $(this).next('.accordion-content');
             
             if ($(this).hasClass('active')) {
+                content.addClass('active');
                 content.css('max-height', content.prop('scrollHeight') + "px");
+                
             } else {
-                content.css('max-height', 0);
-            }
-        });
-    });
-
-    $(document).ready(function() {
-        $('.accordion-a-header').click(function() {
-            
-            $(this).toggleClass('active');
-            
-            var content = $(this).next('.accordion-a-content');
-            
-            if ($(this).hasClass('active')) {
-                content.css('max-height', content.prop('scrollHeight') + "px");
-            } else {
-                content.css('max-height', 0);
+                content.removeClass('active');
+                content.css('max-height', 0);        
             }
         });
     });
